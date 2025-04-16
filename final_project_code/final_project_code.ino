07c3b58bff9c662f2a7630a50ec52c4db3fed137
PontyMython1 <mblitz03@gmail.com>
2025-04-15 20:11:38 -0500

    final commenting and tweaks. Testing code removed.

--- final_project_code/final_project_code.ino
+++ final_project_code/final_project_code.ino
@@ -4,8 +4,8 @@
 
 //Initialize Bluetooth Interface and Data Transfer
 BLEService newService("180A");
-BLEByteCharacteristic readChar("34aaae89-580a-46a3-b1d8-bcd82222bd56", BLERead | BLENotify);
-BLEByteCharacteristic writeChar("6fb9899b-169f-4313-bd0a-7f3c25fb7516", BLEWrite);
+BLEStringCharacteristic readChar("34aaae89-580a-46a3-b1d8-bcd82222bd56", BLERead | BLENotify,100);
+BLEStringCharacteristic writeChar("6fb9899b-169f-4313-bd0a-7f3c25fb7516", BLEWrite,20);
 
 
 //Initialize Variables
@@ -14,11 +14,11 @@ float x, y, z, acc, weight,height,goal,calsperstep,stridelength;
 const int WindowTime = 1;  //Window time in seconds
 float LastSwitch,distancetraveled = 0;
 int FirstZeroIndex,stepcounter = 0;
-const int SamplingRate = 50;  //Sampling rate in Hz
+const int SamplingRate = 40;  //Sampling rate in Hz
 const int zero_buffer = 5;
 const int WindowSize = ceil(WindowTime * SamplingRate);  //Number of Data Point per window
 float accVals[WindowSize] = {1000};
-bool stepchange,calsactive,distactive,goalactive, downstroke,weightreceived,heightreceived,goalreceived,BluetoothRequest,GoalCelebrated = false;
+bool stepchange,calsactive,distactive,goalactive, downstroke,weightreceived,heightreceived,goalreceived,BluetoothRequest,GoalCelebrated,weightrequestprinted,heightrequestprinted,goalrequestprinted = false;
 bool ArrayFull = true;
 LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
 
@@ -57,6 +57,10 @@ void setup() {
 
   BLE.advertise();
   Serial.println("Bluetooth device active");
+  lcd.setCursor(0, 0);
+  lcd.print("Awaiting BLE");
+  lcd.setCursor(0, 1);
+  lcd.print("Connection");
 }
 
 void FindStepWaveform(float* accVals, const int WindowSize, int zero_buffer) {
@@ -81,50 +85,31 @@ void FindStepWaveform(float* accVals, const int WindowSize, int zero_buffer) {
   }
 }
 
-
-//   for (int i = 0; i <= WindowSize - (zero_buffer + 2); i++) {
-//     if (accVals[i] >= 1) {
-//       int zeroCounter = 0;
-//       while ((i + zeroCounter + 1 < WindowSize) && accVals[i + zeroCounter + 1] == 0) {  //while you haven't run out of space and the next uncounted item is also a zero
-//         zeroCounter++;
-//       }
-//       if (zeroCounter >= zero_buffer && (i + zeroCounter + 1 < WindowSize) && accVals[i + zeroCounter + 1] == 1) {
-//         stepcounter++;
-//         // Set the region to 1s (to mark it as processed)
-//         for (int j = 0; j < zeroCounter + 1; j++) {
-//           accVals[i + j] = 1;
-//         }
-//         // Skip over the step region
-//         i += zeroCounter + 1;
-//       }
-//     }
-//   }
-// }
-
 void loop() {
   BLEDevice central = BLE.central();  // Wait for a BLE central
-  bool weightrequestprinted = false;
-  bool heightrequestprinted = false;
-  bool goalrequestprinted = false;
+
   if (central) {
     if(!weightreceived){ //Wait for user to input weight
       if (weightrequestprinted ==false){
         lcd.clear();
         lcd.setCursor(0,0);
-        lcd.print("Input weight (kg)");
+        lcd.print("Input Weight");
+        lcd.setCursor(0,1);
+        lcd.print("(kg)");
         weightrequestprinted = true;
       }
       if (writeChar.written()) {
-        weight = writeChar.value();
+        weight = (writeChar.value()).toFloat();
         Serial.print("Received input number: ");
         Serial.println(weight);
         weightreceived = true;
         lcd.clear();
         lcd.setCursor(0,0);
         lcd.print("Weight Set: ");
+        lcd.setCursor(0,1);
         lcd.print(weight);
         lcd.print("kg");
-        delay(1000);
+        delay(1500);
       }
       
   }
@@ -132,20 +117,26 @@ void loop() {
     if (heightrequestprinted == false){
         lcd.clear();
         lcd.setCursor(0,0);
-        lcd.print("Input height");
+        lcd.print("Input Height");
+        lcd.setCursor(0,1);
+        lcd.print("(cm)");
         heightrequestprinted = true;
     }
     if (writeChar.written()) {
-        height = writeChar.value();
+        height = (writeChar.value()).toFloat();
         Serial.print("Received input number: ");
         Serial.println(height);
         heightreceived = true;
         lcd.clear();
         lcd.setCursor(0,0);
         lcd.print("Height Set: ");
+        lcd.setCursor(0,1);
         lcd.print(height);
         lcd.print("cm");
-        delay(1000);
+        calsperstep = 0.0000023655*weight*height; //calories burned per step
+        stridelength = 0.414 * height/100; //stride length in meters
+        delay(1500);
+        
       }
       
   }
@@ -153,26 +144,27 @@ void loop() {
     if (goalrequestprinted == false){
         lcd.clear();
         lcd.setCursor(0,0);
-        lcd.print("Input Distance Goal (M)");
+        lcd.print("Input Distance");
+        lcd.setCursor(0,1);
+        lcd.print("Goal (M)");
         goalrequestprinted = true;
     }
     if (writeChar.written()) {
-        goal = writeChar.value();
+        goal = (writeChar.value()).toFloat();
         Serial.print("Received input number: ");
         Serial.println(goal);
         goalreceived = true;
         lcd.clear();
         lcd.setCursor(0,0);
         lcd.print("Goal Set: ");
+        lcd.setCursor(0,1);
         lcd.print(goal);
         lcd.print("M");
-        delay(1000);
+        delay(1500);
       }
       
   }
-  //Determine parameter-dependent constants that will be used for further calculations
-      calsperstep = 0.0000023655*weight*height; //calories burned per step
-      stridelength = 0.414 * height/100; //stride length in meters
+      
   }
 if (goalreceived){ // Once the final parameter is received, begin collecting accelerometer date
   delay(floor(1000 / SamplingRate));
@@ -184,9 +176,9 @@ if (goalreceived){ // Once the final parameter is received, begin collecting acc
   int16_t y = Wire.read() | (Wire.read() << 8);  // Parse y values
   int16_t z = Wire.read() | (Wire.read() << 8);  // Parse z values
 
-  x = x / 256;
-  y = y / 256;
-  z = z / 256;
+  x = x / 256; //Intentional integer division digitally improves SNR by subduing noise
+  y = y / 256; //Intentional integer division digitally improves SNR by subduing noise
+  z = z / 256; //Intentional integer division digitally improves SNR by subduing noise
   acc = sqrt((x * x + y * y + z * z));
 }
 
@@ -219,13 +211,18 @@ if (goalreceived){ // Once the final parameter is received, begin collecting acc
     calsactive = true;
     
     Serial.print("Connected to central: ");
-    Serial.println(central.address());  // print the central's BT address
-    readChar.writeValue(stepcounter);
-    Serial.println("Steps sent to Bluetooth");
+    Serial.println(central.address());
+    distancetraveled = stridelength * stepcounter;
+    readChar.writeValue("Steps: " + String(stepcounter) +"\n" +
+                    " | Distance: " + String(distancetraveled)+"\n" +
+                    " | Calories: " + String(calsperstep * stepcounter)+"\n" +
+                    " | Goal Progress: " + String(distancetraveled / goal * 100) + "%");
+
+    Serial.println("Data sent to Bluetooth");
      
   }
 
-      float GoalPercent = distancetraveled/goal;
+      float GoalPercent = distancetraveled/goal; //"Celebration" functionality logic
       if (GoalPercent >= 1 && !GoalCelebrated){
         for (int i=0; i<7; i++){
           lcd.clear();
@@ -276,24 +273,5 @@ if (goalreceived){ // Once the final parameter is received, begin collecting acc
     
     LastSwitch = millis();
   }
+  Serial.println(acc);
 }
-
-
-
-// for (int i = 0; i < WindowSize;i++){
-
-//   Serial.print(accVals[i]);
-//   Serial.print(",");
-// }
-// Serial.println("END Data Point Acquisition");
-//Serial.println("");
-//Serial.println("");
-
-//Serial.print("x = "); // Print values
-//Serial.println(x);
-//Serial.print(", y = ");
-// Serial.println(y);
-//Serial.print(", z = ");
-//Serial.println(z);
-//Serial.print(", acc = ");
-//Serial.println(acc);
